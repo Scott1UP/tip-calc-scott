910add52d074c631a01b762e6123dc8f81ddb28f
Scott1UP <scstudies.20@gmail.com>
2024-04-16 21:51:37 +0100

    JS started.

---
+++ index.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector("#billField");
+const tipAmountGroup = document.querySelector(".tip-amount-group");
+const numberOfPeople = document.querySelector("#peopleField");
+
+const tipAmountToDisplay = document.querySelector("#tipAmountPerPerson");
+const tipTotalToDisplay = document.querySelector("#tipTotal");
+
