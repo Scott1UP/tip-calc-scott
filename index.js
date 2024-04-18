4ccdf4ce67a332569963be87e95ceb8175637fae
Scott1UP <scstudies.20@gmail.com>
2024-04-18 21:44:51 +0100

    JS started.

--- index.js
+++ index.js
@@ -3,5 +3,20 @@ const tipAmountGroup = document.querySelector(".tip-amount-group");
 const numberOfPeople = document.querySelector("#peopleField");
 
 const tipAmountToDisplay = document.querySelector("#tipAmountPerPerson");
-const tipTotalToDisplay = document.querySelector("#tipTotal");
+const totalToDisplay = document.querySelector("#total");
+const resetBtn = document.querySelector("#reset");
 
+numberOfPeople.addEventListener("blur", event => {
+    tipAmountToDisplay.textContent = `$${(billAmount.value / numberOfPeople.value)}`;
+});
+
+numberOfPeople.addEventListener("blur", event => {
+    tipAmountToDisplay.textContent = `$${(billAmount.value / numberOfPeople.value)}`;
+});
+
+resetBtn.addEventListener("click", event => {
+    billAmount.value = "";
+    numberOfPeople.value = "";
+    tipAmountToDisplay.textContent = "$0.00"
+    totalToDisplay.textContent = "$0.00"
+});
