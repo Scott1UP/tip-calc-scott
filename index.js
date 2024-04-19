f7e22ea454d0aebd7bb5af1215715c1b1a62b2a2
Scott1UP <scstudies.20@gmail.com>
2024-04-19 21:28:22 +0100

    JS making progress. Little bit more to go.

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 const billAmount = document.querySelector("#billField");
-const tipAmountGroup = document.querySelector(".tip-amount-group");
 const numberOfPeople = document.querySelector("#peopleField");
 
-const tipAmountToDisplay = document.querySelector("#tipAmountPerPerson");
-const totalToDisplay = document.querySelector("#total");
+const tipAmountPerPerson = document.querySelector("#tipAmountPerPerson");
+const totalPerPerson = document.querySelector("#total");
+
+const tipButtons = document.querySelectorAll(".tip-btn");
 const resetBtn = document.querySelector("#reset");
 
-numberOfPeople.addEventListener("blur", event => {
-    tipAmountToDisplay.textContent = `$${(billAmount.value / numberOfPeople.value)}`;
+let activeTipValue = 0;
+let tipAmount = 0;
+
+tipButtons.forEach(button => {
+  button.addEventListener("click", event => {
+
+    // Remove the 'active' class from all buttons
+    tipButtons.forEach(btn => btn.classList.remove("active"));
+
+    // Add the 'active' class to the clicked button
+    event.target.classList.add("active");
+
+    // Store the value of the active button without the "%" symbol
+    activeTipValue = event.target.value.replace("%", "");
+
+    // Store the tipAmount
+    tipAmount = billAmount.value / activeTipValue;
+
+    // Transform the active tip value to a usable multiplier
+    activeTipValue = activeTipValue / 100 + 1;
+
+    // Log the tip-related values to console to check
+    console.log(activeTipValue);
+    console.log(tipAmount);
+  });
 });
 
+
 numberOfPeople.addEventListener("blur", event => {
-    tipAmountToDisplay.textContent = `$${(billAmount.value / numberOfPeople.value)}`;
+    totalPerPerson.textContent = `$${(billAmount.value * activeTipValue / numberOfPeople.value).toFixed(2)}`;
+    tipAmountPerPerson.textContent = `$${(tipAmount / numberOfPeople.value).toFixed(2)}`;
 });
 
 resetBtn.addEventListener("click", event => {
     billAmount.value = "";
     numberOfPeople.value = "";
-    tipAmountToDisplay.textContent = "$0.00"
-    totalToDisplay.textContent = "$0.00"
+    document.querySelector(".tip-btn.active").classList.remove("active");
+    tipAmountPerPerson.textContent = "$0.00";
+    totalPerPerson.textContent = "$0.00";
 });
